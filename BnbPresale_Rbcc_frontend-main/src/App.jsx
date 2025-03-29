52f11fa02c5202645d2b700f7ae6b6d6c9896f2a
David <aleksandarkolkat@gmail.com>
2025-03-29 17:38:21 +0900

    main__marquee add

--- BnbPresale_Rbcc_frontend-main/src/App.jsx
+++ BnbPresale_Rbcc_frontend-main/src/App.jsx
@@ -36,7 +36,20 @@ function App() {
                 <div></div>
             </div>
 
+            {/* <div class="relative z-50 py-2 overflow-hidden text-black bg-yellow-400 bg-yellow">
+                <div class="flex w-full space-x-8 animate-marquee ">
+                    <p class="whitespace-nowrap">🚀Join Remittix's 100x Moonshot ICO Today And Secure 30% Bonus when you use code: <strong>RTX30P</strong></p>
+                    <p class="whitespace-nowrap">🚀Join Remittix's 100x Moonshot ICO Today And Secure 30% Bonus when you use code: <strong>RTX30P</strong></p>
+                </div>
+            </div> */}
+
             <div className='main__content'>
+                <div class="header_margin_range">
+                </div>
+                <marquee class="main__marquee" direction="left" scrollamount="20">
+                    🚀Join Remittix's 100x Moonshot ICO Today And Secure 30% Bonus when you use code: <strong>RTX30P</strong>
+                    🚀Join Remittix's 100x Moonshot ICO Today And Secure 30% Bonus when you use code: <strong>RTX30P</strong>
+                </marquee>                    
                 <Presale></Presale>
                 {/* <Routes> */}
                 {/* <Route path="/" element={<Presale />}></Route> */}
