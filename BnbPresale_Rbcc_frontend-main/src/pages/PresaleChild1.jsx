50fa30e5f6b42190e416303d585e2fc131baa63f
David <aleksandarkolkat@gmail.com>
2025-03-29 18:40:14 +0900

    separater added

--- BnbPresale_Rbcc_frontend-main/src/pages/PresaleChild1.jsx
+++ BnbPresale_Rbcc_frontend-main/src/pages/PresaleChild1.jsx
@@ -8,6 +8,9 @@ import "react-sweet-progress/lib/style.css";
 function PresaleChild1() {
     return (
         <>
+            <div className="mint_child1_container_separator">
+                <svg width="100%" height="3" viewBox="0 0 1270 3" fill="none" xmlns="http://www.w3.org/2000/svg"><line y1="1.97125" x2="1270" y2="1.97125" stroke="url(#paint0_linear_1541_128)" stroke-opacity="0.24" stroke-width="2.05751"></line><line x1="131.233" y1="1.97125" x2="512.233" y2="1.97125" stroke="url(#paint1_linear_1541_128)" stroke-opacity="0.68" stroke-width="2.05751"></line><defs><linearGradient id="paint0_linear_1541_128" x1="0" y1="3.5" x2="1270" y2="3.5" gradientUnits="userSpaceOnUse"><stop stop-color="#F9FF38" stop-opacity="0"></stop><stop offset="0.178272" stop-color="#F9FF38"></stop><stop offset="1" stop-color="#F9FF38" stop-opacity="0"></stop></linearGradient><linearGradient id="paint1_linear_1541_128" x1="131.233" y1="3.5" x2="512.233" y2="3.5" gradientUnits="userSpaceOnUse"><stop stop-color="#F9FF38" stop-opacity="0"></stop><stop offset="0.178272" stop-color="#F9FF38"></stop><stop offset="1" stop-color="#F9FF38" stop-opacity="0"></stop></linearGradient></defs></svg>
+            </div>
             <div className="mint_child1_container">
                 <div className="mint_child1_pane">
                     <div className="mint_child1_pane_wrapper">
