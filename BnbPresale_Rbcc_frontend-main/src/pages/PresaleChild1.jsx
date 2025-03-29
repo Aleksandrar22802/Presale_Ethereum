36638762869fb3a8214e04007cad66a0d4d19d63
David <aleksandarkolkat@gmail.com>
2025-03-29 18:30:46 +0900

    presale chile 1 has been added

--- BnbPresale_Rbcc_frontend-main/src/pages/PresaleChild1.jsx
+++ BnbPresale_Rbcc_frontend-main/src/pages/PresaleChild1.jsx
@@ -1,26 +1,43 @@
 import React, { useEffect, useRef, useState } from "react";
 
-import IconRemit from "../assets/remittix/logo.svg"
-import IconMint from "../assets/icons/icon-mint.png"
-import IconBoost from "../assets/icons/icon-boost.png"
-import IconEther1 from "../assets/remittix/eth.png"
-import IconEther from "../assets/remittix/eth-eth.png"
-import IconUSDT from "../assets/remittix/usdt-eth.png"
-import IconCARD from "../assets/remittix/card.png"
+import { CheckCircle, Login, Logout, KeyboardArrowRight } from "@mui/icons-material";
+import IntroVideoMap from "../assets/remittix/intro_video_map.png"
 
 import "react-sweet-progress/lib/style.css";
 
 function PresaleChild1() {
     return (
         <>
-            <div className="mint_child1container">
-                <div className="mint_child1pane">
-                    <div className="mint_child1wrapper">
-                        <div className="mint_child1content">
-                        </div>
+            <div className="mint_child1_container">
+                <div className="mint_child1_pane">
+                    <div className="mint_child1_pane_wrapper">
+                        <img 
+                            src={IntroVideoMap}
+                        />
                     </div>
                 </div>
-                <div className="mint_child1dsc">
+                <div className="mint_child1_dsc">
+                    <div className="line_desc_1">
+                        <p>Why Remittix?</p>
+                    </div>
+                    <div className="line_desc_2">
+                        <p>Bridging crypto with local</p>
+                    </div>
+                    <div className="line_desc_3">
+                        <p>payment networks globally.</p>
+                    </div>
+                    <div className="line_desc_4">
+                        <p>Remittix empowers crypto holders and businesses to facilitate crypto-to-fiat transactions worldwide, leveraging local payment networks and blockchain technology.</p>
+                    </div>
+                    <div className="line_desc_5">
+                        <p>At last, enjoy the speed of crypto with the everyday convenience of fiat payments.</p>
+                    </div>
+                    <div className="line_desc_6">
+                        <a className="link_white_paper" target="_blank" href="https://remittix-organization.gitbook.io/remittix">
+                            <span className="text-center">Whitepaper</span>
+                            <KeyboardArrowRight color="white" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
